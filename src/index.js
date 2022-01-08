fa153786bb1cfd161c8454c707abe25fb6f8d42c
Gabriel Nakaema <gsnakaema@gmail.com>
2022-01-08 10:06:33 -0300

    fix: PUT repository

--- src/index.js
+++ src/index.js
@@ -30,9 +30,9 @@ app.post("/repositories", (request, response) => {
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { likes:_, ...updatedRepository } = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
