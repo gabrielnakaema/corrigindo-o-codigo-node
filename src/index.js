498e2c7cf32b932a478a4aa398c895dcd3c7e7e7
Gabriel Nakaema <gsnakaema@gmail.com>
2022-01-08 10:10:27 -0300

    fix: likes

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -70,7 +70,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json({ likes });
 });
 
 module.exports = app;
